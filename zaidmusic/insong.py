bc92132bd71f009da1a2bb2e05334c1ed9c78aa2
attack-90 <76149503+attack-90@users.noreply.github.com>
2021-10-15 21:37:11 +0300

    Update insong.py
    Attack

--- zaidmusic/insong.py
+++ zaidmusic/insong.py
@@ -10,7 +10,7 @@ async def songs(client, message):
     try:
         if len(message.command) < 2:
             await message.reply_text(
-                "❗ **song not found.**\n\n**please give a valid song name.**"
+                "❗ **.**\n\n**please give a valid song name.**"
             )
             return
         text = message.text.split(None, 1)[1]
@@ -19,4 +19,4 @@ async def songs(client, message):
             message.chat.id, results.query_id, results.results[0].id
         )
     except Exception:
-        await message.reply_text("❗ **song not found.**")
+        await message.reply_text("❗ ** wan so wayay o cad😁.**")
