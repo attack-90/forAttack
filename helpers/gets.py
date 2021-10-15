3d2d6ea82b5e9a1e2d27085e300f512b2af1ab3c
attack-90 <76149503+attack-90@users.noreply.github.com>
2021-10-15 20:22:10 +0300

    Update gets.py

--- helpers/gets.py
+++ helpers/gets.py
@@ -31,7 +31,7 @@ def get_url(message_1: Message) -> Union[str, None]:
 
 
 def get_file_name(audio: Union[Audio, Voice]):
-    return f'{audio.file_unique_id}.{audio.file_name.split(".")[-1] if not isinstance(audio, Voice) else "ogg"}'
+    return f'{audio.file_unique_id}.{audio.file_name.split(".")[-1] if not isinstance(audio, Voice) else "ATTACK"}'
 
 
 async def convert_count(count):
@@ -110,4 +110,4 @@ def bytes(size: float) -> str:
 
 
 async def ass_det(assistant: int):
-    print("😎 ZAID MUSIC CORPORATION 😎")
+    print("🥰 Developer by ATTACK🥰")
